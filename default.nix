8d5814373218b400877d53301406efbb5f5b1bc0
iogrt <duarte.dd.dias@gmail.com>
2023-09-30 01:37:50 +0100

    Fix minor nix issue

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc810"}:
+{ pkgs ? import <nixpkgs> {}, hsPkgs ? pkgs.haskell.packages.ghc810}:
 let
   #hsPkgs =  pkgs.haskellPackages;
-  hsPkgs = pkgs.haskell.packages.${compiler}; # 
   pkg = hsPkgs.callCabal2nix "invertible-syntax" ./. {  };
   # was giving errors due to lorri stuff
   # shell = 
